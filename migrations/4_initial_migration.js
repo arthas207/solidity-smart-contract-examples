e3dd0c07630baab7cb83073f263ba0e3008db794
arthas207 <arthas207@yeah.net>
2022-05-15 23:17:11 +0800

    submit
    submit

--- migrations/4_initial_migration.js
+++ migrations/4_initial_migration.js
@@ -1,7 +1,5 @@
 var MyContract = artifacts.require("MyNFT");
-var MyContract1 = artifacts.require("MyToken");
 module.exports = function(deployer) {
   // 部署步骤
   deployer.deploy(MyContract);
-  deployer.deploy(MyContract1);
 };
